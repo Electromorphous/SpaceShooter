474231dae4375a8411bdfa066cfeefccc86717f9
Electromorphous <pstation.474@gmail.com>
2020-01-28 02:54:39 +0530

    this is the final version of the project uploaded on itch.io and simmer.io

---
+++ Assets/Scripts/Link.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour 
+{
+
+	public void OpenSpritesLink()
+	{
+		#if !UNITY_EDITOR
+				openWindow("https://opengameart.org/content/space-shooter-redux");
+		#endif
+	}
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+
+}
